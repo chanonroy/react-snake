8a6dba093244821af9c4ce5cc33fde45919f6fb9
chanonroy <chanonroy@gmail.com>
2017-06-01 18:01:23 -0400

    improved start/stop game and game over logic

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -11,69 +11,46 @@ export class Snake extends React.Component {
 
     this.state = {
       start: false,
+      interval_id: null,
+      board: {
+        max_height: props.board.height,
+        max_width: props.board.width
+      },
       position: {
         height: Math.floor(props.board.height / 2),
         width: Math.floor(props.board.width / 2),
       },
       direction: 'right',
-      speed: 70,
+      speed: 65,
     };
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.snakeMove = this.snakeMove.bind(this);
+    this.move_snake = this.move_snake.bind(this);
+    this.toggle_game = this.toggle_game.bind(this);
   }
+
+  render() {
+    return (
+        <Rect
+            x={this.state.position.width}
+            y={this.state.position.height}
+            width={10}
+            height={10}
+            fill='gold'
+        />
+    )
+  }
+
+  // ---- LIFEYCLE HOOKS ----
 
   componentDidMount() {
     window.addEventListener("keydown", this.handleKeyPress);
-    setInterval(this.snakeMove, this.state.speed);
   }
 
-  snakeMove() {
-    var direction = this.state.direction;
-
-    if (direction === 'up') {
-      this.setState(() => {
-        return {
-          position: {
-            height: this.state.position.height - 10,
-            width: this.state.position.width
-          }
-        }
-      })
-    } else if (direction === 'right') {
-      this.setState(() => {
-        return {
-          position: {
-            height: this.state.position.height,
-            width: this.state.position.width + 10
-          }
-        }
-      })
-    } else if (direction === 'down') {
-      this.setState(() => {
-        return {
-          position: {
-            height: this.state.position.height + 10,
-            width: this.state.position.width
-          }
-        }
-      })
-    } else if (direction === 'left') {
-      this.setState(() => {
-        return {
-          position: {
-            height: this.state.position.height,
-            width: this.state.position.width - 10
-          }
-        }
-      })
-    } else if (direction === 'pause') {
-      clearInterval(this.snakeMove);
-    }
-
-  }
+  // ---- METHODS RE: SNAKE MOVEMENT ----
 
   handleKeyPress(event) {
+
     // For validation that snake can't go back on itself
     var current_dir = this.state.direction;
 
@@ -89,27 +66,85 @@ export class Snake extends React.Component {
     } else if ((event.key == 'ArrowLeft' || event.key == 'a') && current_dir !== 'right') {
       this.setState(() => { return { direction: 'left' } })
 
-    } else if (event.key == ' ') {
-      this.setState(() => { return { direction: 'pause' } })
+    } else if (event.key == 'Enter' || event.key == ' ') {
+      if (this.state.start === false) {
+        this.setState(() => {
+          return { start: true }
+        })
+        this.toggle_game('on');
+      }
     }
   }
 
-  gameOver() {
-    clearInterval(this.snakeMove);
-    alert('Game Over');
+  move_snake() {
+    var direction = this.state.direction;
+    var current_height = this.state.position.height;
+    var current_width = this.state.position.width;
+    var max_height = this.state.board.max_height;
+    var max_width = this.state.board.max_width;
+
+    // Snake hits the edge - Game Over
+    if (current_width < 0 || current_width >= max_width) {
+      return this.toggle_game('off');
+    } else if (current_height < 0 || current_height >= max_height) {
+      return this.toggle_game('off');
+    }
+
+    // Snake moves in a certain direction
+    if (direction === 'up') {
+      this.setState(() => {
+        return {
+          position: { height: current_height - 10, width: current_width }
+        }
+      })
+    } else if (direction === 'right') {
+      this.setState(() => {
+        return {
+          position: { height: current_height, width: current_width + 10 }
+        }
+      })
+    } else if (direction === 'down') {
+      this.setState(() => {
+        return {
+          position: { height: current_height + 10, width: current_width }
+        }
+      })
+    } else if (direction === 'left') {
+      this.setState(() => {
+        return {
+          position: { height: current_height, width: current_width - 10 }
+        }
+      })
+    }
+
   }
 
-  render() {
-    return (
-        <Rect
-            x={this.state.position.width}
-            y={this.state.position.height}
-            width={10}
-            height={10}
-            fill='gold'
-        />
-    )
+  // ---- METHODS RE: STATE OF GAME ----
+
+  toggle_game(str) {
+    // str = 'on', 'off'
+
+    if (str === 'on') {
+      var intervalID = setInterval(this.move_snake, this.state.speed);
+      this.setState(() => {
+        return { interval_id: intervalID }
+      })
+    } else if (str === 'off') {
+      alert('Game Over');
+      clearInterval(this.state.interval_id);
+      this.setState(() => {
+        return {
+          start: false,
+          position: {
+            height: Math.floor(this.state.board.max_height / 2),
+            width: Math.floor(this.state.board.max_width / 2),
+          },
+        }
+      })
+    }
+
   }
+
 }
 
 Snake.propTypes = {
